d2e845c2d222dc8d2b0f5a4184f129b210d4b843
Ioannis <powerfulguy4774@gmail.com>
2023-10-09 09:27:44 +0200

    Starting on the frontend

---
+++ frontend/src/components/single-entry.component.js
@@ -0,0 +1,43 @@
+import React, { UseState, useEffect } from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Button, Card, Row, Col } from 'react-bootstrap';
+
+const Entry = ({ entryData, setChangeIngredient, deleteSingleEntry, setChangeEntry }) => {
+    return (
+        <Card>
+            <Row>
+                <Col>Dish:{entryData !== undefined && entryData.dish}</Col>
+                <Col>Ingredients:{entryData !== undefined && entryData.ingredients}</Col>
+                <Col>Calories:{entryData !== undefined && entryData.calories}</Col>
+                <Col>Protein:{entryData !== undefined && entryData.protein}</Col>
+                <Col>Carbs:{entryData !== undefined && entryData.carbs}</Col>
+                <Col>Fat:{entryData !== undefined && entryData.fat}</Col>
+                <Col><Button onClick={() => deleteSingleEntry(entryData._id)}>Delete Entry</Button></Col>
+                <Col><Button onClick={() => changeIngredient()}>Change Ingredients</Button></Col>
+                <Col><Button onClick={() => changeEntry()}>Change Entry</Button></Col>
+            </Row>
+        </Card>
+    )
+
+    function changeIngredient() {
+        setChangeIngredient(
+            {
+                "change": true,
+                "id": entryData._id
+            }
+        )
+    }
+
+    function changeEntry() {
+        setChangeEntry(
+            {
+                "change": true,
+                "id": entryData._id
+            }
+        )
+    }   
+}
+
+export default Entry;
